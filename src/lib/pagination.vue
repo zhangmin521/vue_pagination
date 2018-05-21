67e22367674e860c4620324c478d3ddb0624f7b8
zhangmin <zhangmin@channelsoft.com>
2018-05-21 15:56:03 +0800

    更新图片

--- src/lib/pagination.vue
+++ src/lib/pagination.vue
@@ -6,12 +6,14 @@
     </select>
     <button class="preButton" @click="prePage" :disabled="params.currentPage==1" :class="params.currentPage==1?'notAllowed':''">
       <img src="./pre.png" alt="">
+      <!--<img class="preI">-->
     </button>
     <ul class="pageWrap">
       <li @click="curPage(item)" class="perPage" :class="{activePage:item==params.currentPage}" v-for="item in pageList" track-by="$index">{{item}}</li>
     </ul>
     <button class="nextButton" @click="nextPage" :disabled="params.currentPage==Math.ceil(params.totalSize/params.perSize)" :class="params.currentPage==Math.ceil(params.totalSize/params.perSize)?'notAllowed':''">
       <img src="./next.png" alt="">
+      <!--<img class="nextI">-->
     </button>
     <span class="jumpTip">跳至</span><input class="jumpInput" type="text" v-model="jumpPage" @change="changeCur" onkeyup="this.value=this.value.replace(/[^0-9]/g,'')" onafterpaste="this.value=this.value.replace(/[^0-9]/g,'')"><span class="jumpTip">页</span>
   </div>
@@ -158,6 +160,16 @@
   .preButton img, .nextButton img{
     width: 7px;
   }
+  /*.preI, .nextI{
+    width: 14px;
+    height: 26px;
+  }
+  .preI{
+    background: url('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAA4AAAAaCAYAAACHD21cAAAAAXNSR0IArs4c6QAAAh5JREFUOBGVlM9LG1EQx91V0WatNaQ55Zh7sbRBRBF/0KrJJkVaeiiElh5yi0dPHoLgyaun4CEH8WAENbtrrRpsD4VCSg9e+z/0VKK0+eFn1F1W3STbB5O3+958Zr47816Urv8YqVTqaa1WKyqK8lP1yyUSiWf1er2MfxTTFD9gMpmMNRqNo2azOYT/92AwONsRjMfjIzh/xh5hXzVN04vF4p+2UnVdH8X56AY6BpoXiPeuliDyxpEmmQYxIxqNJoHOBZLhKRV5E1TOAhxg3olEIu/y+fy/a+T69x6IvEkKYbKtAW0GAoEPZKq7IXm+JRVohiyWQNhGLBZ77wUJ6GSkuS/o0z7gAzKtm6a5yNwUJ69xBfJNc2zuYv2qqq4BLXk5u9dUqveShT2ByLDiB5IAPcjbYO4DWrYsa1UW/Qwpjl2x4Vwu1+MHEh+VTB+xc4ryplKpbGcymV4/8FVx+M5pemcABwiyT+/e0oa/7QI47XA3HsDkXL5uBzsHgGp+Ids8JodYr1aru9lstq9VViej7cBBGKPSn5D9kCCH4XB4oVAoXNj79nwPlA25TnzzIY9yM+Q6vUK2czPExxOUDfcFJnOZgnW+VgLKQPZzZB8jewj4lOOoG4ZRlT2nOPJyd5RKpR84z7D+G3iKIAfpdFpuTmup7iDIHibjCXCI9W+hUKjzn5UdANlPyFgGfszaVlupNiQzss+Ypsj8i7n7EnS+0Qej7kcMAAAAAElFTkSuQmCC');
+  }
+  .nextI{
+    background: url('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAA4AAAAaCAYAAACHD21cAAAAAXNSR0IArs4c6QAAAiFJREFUOBGNlM9LG1EQx7O7qf2xRdO/IZB7LTmUFlq1tobdVYTmUDA0p5yaY68lvXr0KB48CTYUaXYN/qgiQlGh0NJrzjnFFG1NbKGb9DMhG6Ju1h0Y3uzO+7z5vvdmVzEM40O73R6NRqPpUqn0LRLSVObpeNx13R0WeRCSi6ixWOwVkw+peo/xs2VZyTCwIpPS6fTdRqPhED7BT/EX5XL5iHGgidRIsVg803U9RbiNj+Bbpmk+ZBxoHVCywOfxeNwitPFhpG8i+7Hk/KwjtT+Ry+VuVKvVFcCXiqKcMRrI3u+fI/EVUF6yZ63ZbC4DzfHYUFXVdBxnT3Ke9aR6L2REtptMJl8TLuE6C6yz5wnJeeZb0UsCKAALjG+Qfa5p2gxNIgfoL9UDvRF4vtVqveX5Dz7LnjcCK3qgjHTVeyq/I/xL5WmtPxkUVyqVvUQi8Y85z1ngqe/h+C1QKBSiAPe7OTeU1O7drgLOyiEBG9eC3OkQdyqf3gxQkzu1bNveDQQFovk/UsHELzTCwD3m8/mbVFoTSFoPT/V3j2/FbDZ7q1arrSFvCuA3x5/i4r+wSM+ugMi7jbxPzJjEf7GnKSod9IhucAEUCHk2laQvAz/oHsi3d4e2coDGkHeCvEnkfb1cyXvuHE4mk9H5WZUFIvETeRNBkMCKQPV6fZP4EZXqwM9o4u+SDDIVaJEJAh0jbzwMJAuKVA2owjiGvB/yMoz9BwJM8ZrOIl5tAAAAAElFTkSuQmCC');
+  }*/
   .perPage{
     float: left;
     display: inline-block;
